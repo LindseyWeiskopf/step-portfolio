14fdcab2e8fd17c6ea2f47ebaa34bba7d36dc760
Lindsey Weiskopf <ltweiskopf@gmail.com>
2020-06-01 17:52:23 -0400

    Add id as instance variable

--- portfolio/src/main/java/com/google/sps/data/Post.java
+++ portfolio/src/main/java/com/google/sps/data/Post.java
@@ -1,11 +1,13 @@
 package com.google.sps.data;
 
 public final class Post {
+  private final long id;
   private final String name;
   private final String email;
   private final String comment;
 
-  public Post(String name, String email, String comment) {
+  public Post(long id, String name, String email, String comment) {
+    this.id = id;
     this.name = name;
     this.email = email;
     this.comment = comment;
