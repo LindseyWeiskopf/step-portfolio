0ed867a708c53a00f325cc698425da26c2f26ad5
Lindsey Weiskopf <ltweiskopf@gmail.com>
2020-06-02 18:34:36 +0000

    Add timestamp as instance variable

--- portfolio/src/main/java/com/google/sps/data/Post.java
+++ portfolio/src/main/java/com/google/sps/data/Post.java
@@ -5,11 +5,13 @@ public final class Post {
   private final String name;
   private final String email;
   private final String comment;
+  private final long timestamp;
 
-  public Post(long id, String name, String email, String comment) {
+  public Post(long id, String name, String email, String comment, long timestamp) {
     this.id = id;
     this.name = name;
     this.email = email;
     this.comment = comment;
+    this.timestamp = timestamp;
   }
 }
