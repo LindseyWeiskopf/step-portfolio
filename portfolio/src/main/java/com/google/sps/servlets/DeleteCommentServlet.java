e3c4a48b56e7f2ab86eb423eb3a9937bbd60c126
Lindsey Weiskopf <ltweiskopf@gmail.com>
2020-06-04 15:08:44 +0000

    Change imported files

--- portfolio/src/main/java/com/google/sps/servlets/DeleteCommentServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteCommentServlet.java
@@ -2,13 +2,20 @@ package com.google.sps.servlets;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.Entity;
 import java.io.IOException;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.gson.Gson;
+import com.google.sps.data.Post;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.appengine.api.datastore.FetchOptions;
 
 /** Servlet responsible for deleting comments. */
 @WebServlet("/delete-comment")
