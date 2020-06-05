62ca7d1e6bab89efcac15b54394bd3c0e3fba1e8
Lindsey Weiskopf <ltweiskopf@gmail.com>
2020-06-05 20:55:29 +0000

    Create scrap dropoff class to represent coords of dropoff locations

---
+++ portfolio/src/main/java/com/google/sps/data/ScrapDropoff.java
@@ -0,0 +1,13 @@
+package com.google.sps.data;
+
+// Represents the physical location of a Food scrap Dropoff in NYC
+public final class ScrapDropoff {
+  
+  private double lat;
+  private double lng;
+
+  public ScrapDropoff(double lat, double lng) {
+    this.lat = lat;
+    this.long = lng;
+  }
+}
