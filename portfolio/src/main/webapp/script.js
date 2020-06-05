a43d9e2ba706cfab7619becd68b74d6b58223a3b
Lindsey Weiskopf <ltweiskopf@gmail.com>
2020-06-05 18:23:00 +0000

    Deleted extra spaces

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -18,7 +18,6 @@ function getComments() {
   document.getElementById('greeting-container').innerHTML = "";
   var commentNum = parseInt(document.getElementById('quantity').value);
   
-
   fetch('/comments?quantity=' + commentNum).then(response => response.json()).then((posts) => {
       const liElement = document.getElementById('greeting-container');
       posts.forEach((post) => {
