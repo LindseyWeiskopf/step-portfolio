cd27a0f9fe735337e2956b5280ee52406549e8bf
Lindsey Weiskopf <ltweiskopf@gmail.com>
2020-06-09 18:43:21 +0000

    Add translating aspect of code

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -21,6 +21,9 @@ import java.io.IOException;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.TranslateOptions;
+import com.google.cloud.translate.Translation;
 import com.google.gson.Gson;
 import com.google.sps.data.Post;
 import java.util.ArrayList;
@@ -53,6 +56,7 @@ public class DataServlet extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     Entity commentEntity = createEntity(request);
+  
 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     datastore.put(commentEntity);
@@ -108,12 +112,24 @@ public class DataServlet extends HttpServlet {
       long timestamp = (long) entity.getProperty("timestamp");
       String name = (String) entity.getProperty("name");
       String email = (String) entity.getProperty("email");
-      String comment = (String) entity.getProperty("comment");
-      
+      //String comment = (String) entity.getProperty("comment");
+      String comment = translateComment(request, entity);
       Post post = new Post(id, name, email, comment, timestamp);
      
       posts.add(post);
     }  
     return posts;  
+  }
+
+  private String translateComment(HttpServletRequest request, Entity entity) {
+    String originalComment = (String) entity.getProperty("comment");
+    String languageChoice = getLanguage(request);
+
+    // Do the translation.
+    Translate translate = TranslateOptions.getDefaultInstance().getService();
+    Translation translation =
+        translate.translate(originalComment, Translate.TranslateOption.targetLanguage(languageChoice));
+    String translatedText = translation.getTranslatedText();
+    return translatedText;
   }
 }
